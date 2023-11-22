10686f3ae3dcafa32668587c8b0ce1b34ae83fac
Tran Van Nam <namintelvn@gmail.com>
2023-11-22 17:46:20 +0700

    :sparkles: format for input text

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def on_submit_edit():
     selected_iid = trans_input.editing_item_iid
     selected_index = trans_input.editing_item_index
 
-    input_value = trans_input.get('1.0', END).strip()
+    input_value = trans_input.get('1.0', END).strip().replace('\n', '<br>')
     current_values = table.item(selected_iid).get("values")
 
     current_values[2] = input_value
@@ -55,7 +55,7 @@ def on_trans_edit():
     selected_iid = trans_input.editing_item_iid
     current_values = table.item(selected_iid).get("values")
 
-    result = translator.translate(current_values[3], src='en', dest='vi')
+    result = translator.translate(current_values[3].replace('<br>', '\n'), src='en', dest='vi')
     trans_input.delete('1.0', END)
     trans_input.insert(INSERT, result.text)
 
@@ -68,7 +68,41 @@ def on_revert_edit():
     current_values = table.item(selected_iid).get("values")
 
     trans_input.delete('1.0', END)
-    trans_input.insert(INSERT, current_values[2])
+    trans_input.insert(INSERT, current_values[2].replace('<br>', '\n'))
+
+
+def get_indices(string, char, plus=0):
+    indices = []
+    current_line = 0
+    for i, line in enumerate(string.split("<br>")):
+        if char in line:
+            index = 0
+            current_line = i + 1
+            while True:
+                try:
+                    index = line.index(char, index)
+                    indices.append(f"{current_line}.{index + plus}")
+                    index += 1
+                except ValueError:
+                    break
+    return indices
+
+
+def format_text_input(input, value):
+    tag_open_chars = get_indices(value, '<')
+    tag_close_chars = get_indices(value, '>', 1)
+    tag_open_var_chars = get_indices(value, '[')
+    tag_close_var_chars = get_indices(value, ']', 1)
+
+    end_tag = 0
+    for index, val in enumerate(tag_open_chars):
+        input.tag_add("hightlight", f"{val}", f"{tag_close_chars[index]}")
+        end_tag += 1
+        if end_tag % 2 == 0:
+            input.tag_add("hightlight2", f"{tag_close_chars[index-1]}", f"{val}")
+
+    for index, val in enumerate(tag_open_var_chars):
+        input.tag_add("hightlight2", f"{val}", f"{tag_close_var_chars[index]}")
 
 
 def on_edit(event):
@@ -90,12 +124,14 @@ def on_edit(event):
     values = table.item(selected, "values")
 
     trans_input.delete('1.0', END)
-    trans_input.insert(INSERT, values[2])
+    trans_input.insert(INSERT, values[2].replace('<br>', '\n'))
+    format_text_input(trans_input, values[2])
 
     base_input.configure(state='normal')
     base_input.delete('1.0', END)
-    base_input.insert(INSERT, values[3])
+    base_input.insert(INSERT, values[3].replace('<br>', '\n'))
     base_input.configure(state='disabled')
+    format_text_input(base_input, values[3])
 
     if column == "#1":
         pyperclip.copy(values[0])
@@ -406,6 +442,8 @@ def init_trans_area():
     trans_input.configure(wrap="word")
     trans_input['yscrollcommand'] = on_scroll
     trans_input.vbar.config(command=yview)
+    trans_input.tag_config("hightlight", foreground="#0054D2", font=("TkTextFont", 10, "bold"))
+    trans_input.tag_config("hightlight2", foreground="#E9500C", font=("TkTextFont", 10, "bold"))
 
     button_frame = Frame(edit_frame)
     button_frame.pack(side=LEFT, padx=5)
@@ -430,6 +468,8 @@ def init_trans_area():
     base_input.configure(wrap="word")
     base_input['yscrollcommand'] = on_scroll
     base_input.vbar.config(command=yview)
+    base_input.tag_config("hightlight", foreground="#0054D2", font=("TkTextFont", 10, "bold"))
+    base_input.tag_config("hightlight2", foreground="#E9500C", font=("TkTextFont", 10, "bold"))
 
 
 def init_layout():
